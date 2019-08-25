4757eaa40ea8adef314c3386dc5672d038805229
Stephen Feagin <stephenfeagin@gmail.com>
2019-08-25 14:49:14 -0400

    Add README template in new_puzzle.sh

--- new_puzzle.sh
+++ new_puzzle.sh
@@ -17,10 +17,13 @@ if ! [[ $YEAR =~ ^201[5-8]$ ]]; then
 fi
 
 # If DAY is not an integer between 1 and 31, print error and exit
-if ! [[ $DAY =~ ^[012]?[0-9]$ ]] && ! [[ $DAY =~ ^3[01]$ ]]; then
+if ! [[ $DAY =~ ^[12]?[0-9]$ ]] && ! [[ $DAY =~ ^3[01]$ ]]; then
     echo "Invalid <day>. Must be between 1 and 31, inclusive."
     exit 1
 fi
+
+# Create header for new README
+README="# [$YEAR Day $DAY:](https://adventofcode.com/$YEAR/day/$DAY)\n\n## Original Brief\n\n"
 
 # If DAY is one digit, left-pad with a zero
 if [[ $DAY =~ ^[0-9]$ ]]; then
@@ -36,5 +39,7 @@ fi
 
 mkdir -p $DIR
 
+# Copy template files (or variable string) into the new puzzle files
 cp template/input.txt.templ $DIR/input.txt
 cp template/main.go.templ $DIR/main.go
+echo -e $README > $DIR/README.md
